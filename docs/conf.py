aa5c5b154d4d16258ab06f0ea737fff21c33ab90
evaristor <evaro.mx@gmail.com>
2018-07-05 23:28:24 -0700

    Actualiza nombre de proyecto y autor

--- docs/conf.py
+++ docs/conf.py
@@ -193,8 +193,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'manual'),
+  ('index', 'ReadtheDocsTemplate.tex', u'Protocolo para guardar datos en Excel',
+   u'Conservación de Islas', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -223,8 +223,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
-     [u'Read the Docs'], 1)
+    ('index', 'readthedocstemplate', u'Protocolo para guardar datos en Excel',
+     [u'Conservación de Islas'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -237,8 +237,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
+  ('index', 'ReadtheDocsTemplate', u'Protocolo para guardar datos en Excel',
+   u'Conservación de Islas', 'ReadtheDocsTemplate', 'One line description of project.',
    'Miscellaneous'),
 ]
 
